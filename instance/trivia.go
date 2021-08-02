7138ab62b4cc68c859795c599ff6c9649df8a2bc
V4NSH4J <79518089+V4NSH4J@users.noreply.github.com>
2021-08-02 20:26:04 +0530

    trivia fix

--- instance/trivia.go
+++ instance/trivia.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"html"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"path"
 	"path/filepath"
 	"time"
-	"math/rand"
 
 	"github.com/dankgrinder/dankgrinder/discord"
 )
@@ -27,7 +27,6 @@ func (in *Instance) trivia(msg discord.Message) {
 
 	details := exp.trivia.FindStringSubmatch(msg.Embeds[0].Description)[1:]
 	question := details[0]
-
 
 	ex, _ := os.Executable()
 	ex = filepath.ToSlash(ex)
@@ -48,10 +47,13 @@ func (in *Instance) trivia(msg discord.Message) {
 			i := in.returnButtonIndex(res, 4, msg)
 			time.Sleep(1 * time.Second)
 			in.pressButton(i, msg)
-		}else{
-			i := rand.Intn(4)
-			in.pressButton(i, msg)
 		}
 	}
+	for i := 0; i < len(database.Database); i++ {
+		if question != html.UnescapeString(database.Database[i].Question) {
+			return
+		}
+	}
+	i := rand.Intn(4)
+	in.pressButton(i, msg)
 }
-
